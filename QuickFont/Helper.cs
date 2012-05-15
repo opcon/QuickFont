7661cd6a891f38ffb3cc1814a4276ae3a51c81a9
John Marshall <john.m.marshall@gmail.com>
2012-05-15 17:52:39 -0400

    Fixed bug where texture2d was not disabled after creating texture

--- QuickFont/Helper.cs
+++ QuickFont/Helper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using OpenTK.Graphics.OpenGL;
 
 namespace QuickFont
 {
@@ -12,5 +13,34 @@ namespace QuickFont
             collection.CopyTo(output, 0);
             return output;
         }
+
+        /// <summary>
+        /// Ensures GL.End() is called
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <param name="code"></param>
+        public static void SafeGLBegin(BeginMode mode, Action code)
+        {
+            GL.Begin(mode);
+
+            code();
+
+            GL.End();
+        }
+
+        /// <summary>
+        /// Ensures that state is disabled
+        /// </summary>
+        /// <param name="cap"></param>
+        /// <param name="code"></param>
+        public static void SafeGLEnable(EnableCap cap, Action code)
+        {
+            GL.Enable(cap);
+
+            code();
+
+            GL.Disable(cap);
+        }
+
     }
 }
--- QuickFont/TexturePage.cs
+++ QuickFont/TexturePage.cs
@@ -36,24 +36,25 @@ namespace QuickFont
 
         private void CreateTexture(BitmapData dataSource)
         {
-
             width = dataSource.Width;
             height = dataSource.Height;
 
-            GL.Enable(EnableCap.Texture2D);
-            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+            Helper.SafeGLEnable(EnableCap.Texture2D, () =>
+            {
+                GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
 
-            GL.GenTextures(1, out gLTexID);
-            GL.BindTexture(TextureTarget.Texture2D, gLTexID);
+                GL.GenTextures(1, out gLTexID);
+                GL.BindTexture(TextureTarget.Texture2D, gLTexID);
 
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Clamp);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Clamp);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Clamp);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Clamp);
 
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
 
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0,
-                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, dataSource.Scan0);
+                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0,
+                    OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, dataSource.Scan0);
+            });
         }
 
 
