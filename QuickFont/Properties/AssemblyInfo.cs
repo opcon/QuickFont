a3a3e6d5ea7b78365716963dff9ab0c2085b4b6d
Martin Ayasse <martinay@outlook.com>
2015-08-13 15:23:09 +0200

    changed Verision to 3.0.0.2

--- QuickFont/Properties/AssemblyInfo.cs
+++ QuickFont/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QuickFont")]
-[assembly: AssemblyDescription("Quickfont provides support for Text rendering with OpenGL ES 3.0. For more information please have a look at the GitHub repository https://github.com/vescon/QuickFont")]
+[assembly: AssemblyDescription("QuickFont provides support for Text rendering with OpenGL ES 3.0. For more information please have a look at the GitHub repository https://github.com/vescon/QuickFont")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("QuickFont")]
 [assembly: AssemblyProduct("QuickFont")]
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -24,12 +24,13 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.2")]
+[assembly: AssemblyInformationalVersion("3.0.0.2")]
 [assembly: AssemblyFileVersion("3.0.0.2")]
