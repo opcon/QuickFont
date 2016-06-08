3958701fc30345680473d41fb09f8b02e5b56761
Patrick Yates <frozen.dinosaur@gmail.com>
2016-06-08 19:05:52 +1000

    Update nuget release notes

--- QuickFont/_CreateNewNuGetPackage/Config.ps1
+++ QuickFont/_CreateNewNuGetPackage/Config.ps1
@@ -20,7 +20,8 @@ $versionNumber = ""
 
 # Specify any Release Notes for this package. 
 # These will only be included in the package if you have a .nuspec file for the project in the same directory as the project file.
-$releaseNotes = ""
+$releaseNotes = "* Linux and OSX support using SharpFont
+				 * Improved shader loading"
 
 # Specify a specific Configuration and/or Platform to only create a NuGet package when building the project with this Configuration and/or Platform.
 #	e.g. $configuration = "Release"
