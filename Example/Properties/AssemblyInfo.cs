9fb4b0315a75fc780953c7bc4ac9fbb101131f22
Patrick Yates <frozen.dinosaur@gmail.com>
2016-06-23 18:23:15 +1000

    Bump assembly version number to 4.3
    Updated Example assembly properties

--- Example/Properties/AssemblyInfo.cs
+++ Example/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Example")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("QuickFont.Example")]
+[assembly: AssemblyDescription("The QuickFont Example Project")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GitHub")]
-[assembly: AssemblyProduct("Example")]
+[assembly: AssemblyCompany("QuickFont")]
+[assembly: AssemblyProduct("QuickFont.Example")]
 [assembly: AssemblyCopyright("Copyright © QuickFont contributors 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.*")]
+[assembly: AssemblyVersion("4.3.*")]
